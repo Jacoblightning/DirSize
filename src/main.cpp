9c6a5f25eb0d94e253b0831bc43eeb954a46649c
Jacob Freeman <t00niyf5@duck.com>
2025-03-14 21:52:05 -0400

    Change option Parsing

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,15 @@ const char* version = DSIZE_VERSION;
 const char* version = "unknown";
 #endif
 
+struct DS_options_t{
+    bool help = false;
+    bool version = false;
+    bool follow_symlink = false;
+    bool verbose = false;
+    bool nohuman = false;
+    int include_others = 0;
+};
+
 struct HumanReadable
 {
     std::uintmax_t size{};
@@ -44,7 +53,7 @@ void printHelp(const char *progname) {
     "  -v, --verbose         \tbe more verbose\n"
     "  -f, --follow-symlinks \tfollow directory symlinks\n"
     "  -m, --machine-readable\toutput machine readable format\n"
-    ""
+    "      --include-others  \tinclude block and character devices\n"
     ""
     ""
     "  -V, --version         \toutput version information and exit\n\n"
@@ -52,7 +61,7 @@ void printHelp(const char *progname) {
     << std::endl;
 }
 
-void folderSize(const char* folder, const bool verbose, const bool follow_symlinks, const char *progname, const bool nohuman) {
+void folderSize(const char* folder, const char *progname, const DS_options_t& DS_options) {
     if (!std::filesystem::exists(folder)) {
         std::cerr << progname << ": cannot access '" << folder << "': No such file or directory" << std::endl;
         return;
@@ -68,21 +77,21 @@ void folderSize(const char* folder, const bool verbose, const bool follow_symlin
         :
         fs::recursive_directory_iterator(
             folder,
-            (follow_symlinks?fs::directory_options::follow_directory_symlink:fs::directory_options::none)
+            (DS_options.follow_symlink?fs::directory_options::follow_directory_symlink:fs::directory_options::none)
             | fs::directory_options::skip_permission_denied // Fix race condition
             )
     )
     {
         try {
-            if (iter.is_symlink() && ! follow_symlinks) {
-                if (verbose)
+            if (iter.is_symlink() && ! DS_options.follow_symlink) {
+                if (DS_options.verbose)
                     std::cerr << "Not following symlink: " << iter.path() << std::endl;
             } else if (iter.is_directory()) {
-                if (verbose)
+                if (DS_options.verbose)
                     std::cerr << "Traversing into directory: " << iter.path() << std::endl;
             } else {
                 size += iter.file_size();
-                if (verbose)
+                if (DS_options.verbose)
                     std::cerr << "Found file: " << iter.path() << ". Size: " << HumanReadable{iter.file_size()} << std::endl;
             }
         } catch (fs::filesystem_error& e) {
@@ -90,7 +99,7 @@ void folderSize(const char* folder, const bool verbose, const bool follow_symlin
         }
     }
 
-    if (nohuman)
+    if (DS_options.nohuman)
         std::cout << folder << ":" << size << std::endl;
     else
         std::cout << folder << ": " << HumanReadable{size} << std::endl;
@@ -101,20 +110,18 @@ int main(const int argc, char **argv) {
 
     int opt;
 
-    bool help = false;
-    bool follow_symlink = false;
-    bool verbose = false;
-    bool version = false;
-    bool nohuman = false;
+    DS_options_t DS_options;
 
     const option long_options[] = {
-        {"help",             no_argument, nullptr, 'h'},
-        {"machine-readable", no_argument, nullptr, 'm'},
-        {"verbose",          no_argument, nullptr, 'v'},
-        {"version",          no_argument, nullptr, 'V'},
-        {"follow-symlinks",  no_argument, nullptr, 'f'},
+        {"help",             no_argument, nullptr,         'h'},
+        {"machine-readable", no_argument, nullptr,         'm'},
+        {"verbose",          no_argument, nullptr,         'v'},
+        {"version",          no_argument, nullptr,         'V'},
+        {"follow-symlinks",  no_argument, nullptr,         'f'},
+        {"include-others",   no_argument, &DS_options.include_others, 1},
         {nullptr,            0,           nullptr, 0}
     };
+
     // TODO: Handle block and char devices
 
     int errorcode = 0;
@@ -122,38 +129,38 @@ int main(const int argc, char **argv) {
     while ((opt = getopt_long(argc, argv, "hfvmV", long_options, nullptr)) != -1) {
         switch (opt) {
             case 'h':
-                help = true;
+                DS_options.help = true;
                 break;
             case 'f':
-                follow_symlink = true;
+                DS_options.follow_symlink = true;
                 break;
             case 'v':
-                verbose = true;
+                DS_options.verbose = true;
                 break;
             case 'V':
-                version = true;
+                DS_options.version = true;
                 break;
             case 'm':
-                nohuman = true;
+                DS_options.nohuman = true;
                 break;
             default:
                 break;
         }
     }
 
-    if (version) {
+    if (DS_options.version) {
         printVersion(prog);
         return 0;
     }
 
-    if (help) {
+    if (DS_options.help || optind == argc) {
         printHelp(prog);
         return 0;
     }
 
     {
         for (int i = optind; i < argc; i++) {
-            folderSize(argv[i], verbose, follow_symlink, prog, nohuman);
+            folderSize(argv[i], prog, DS_options);
         }
     }
     return errorcode;
